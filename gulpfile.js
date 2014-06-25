9abeb9db41b2b5337f8b58f39579d2be021c817f
drivativ <accounts@drivativ.com>
2014-06-25 05:39:56 -0700

    moved to global style, misc updates

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,9 @@ var concat  = require('gulp-concat');
 var nodemon = require('gulp-nodemon');
 var rename  = require('gulp-rename');
 var sass    = require('gulp-sass');
+var routing = {
+  src: '../continu-app/public/sass'
+}
 
 gulp.task('js', function() {
   gulp.src(['./public/js/libs/angular.min.js',
@@ -13,6 +16,13 @@ gulp.task('js', function() {
             './public/js/controllers.js'])
       .pipe(concat('styleguide.min.js'))
       .pipe(gulp.dest('./public/js'));
+});
+
+gulp.task('sass-global', function () {
+  gulp.src(['../continu-app/public/sass/*.scss'])
+      .pipe(sass({includePaths: ['../continu-app/public/sass'], errLogToConsole: true}))
+      .pipe(rename('style-global.css'))
+      .pipe(gulp.dest('./public/css'));
 });
 
 gulp.task('sass', function () {
